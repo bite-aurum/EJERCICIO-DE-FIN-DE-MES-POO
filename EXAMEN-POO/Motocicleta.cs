611269e7eeafdfa53de455f2fb4192ab6eec3095
AlejandroJS <juanalejandroshago@gmail.com>
2026-06-26 13:26:14 -0600

    Añade clase Motocicleta y actualiza archivos del proyecto
    Se agrega la clase Motocicleta que hereda de Vehiculo, incluyendo propiedades y métodos específicos. Se actualiza el archivo .csproj para incluir Motocicleta.cs y se añade el identificador de proyecto en el archivo de solución.

---
+++ EXAMEN-POO/Motocicleta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAMEN_POO
+{
+    public class Motocicleta: Vehiculo
+    {
+        public int Cilindrada { get; set; }
+        public Motocicleta(int horas, int cilindrada) : base(horas)
+        {
+            Cilindrada = cilindrada;
+        }
+        public Motocicleta() : base()
+        {
+            Cilindrada = 0;
+        }
+        public override double CalcularTarifa()
+        {
+            return horasEstancia * 15.0;
+        }
+        public override string Describir()
+        {
+            return $"Motocicleta {Cilindrada}cc con {horasEstancia} horas de estancia.";
+        }
+
+    }
+}
