9902e79b31aa385c5cf4f0060585cb86c549326b
RafaelMarquez <rafamarquez0106@gmail.com>
2026-06-26 13:19:12 -0600

    Añadir clase Camioneta y actualizar archivos de proyecto
    Se incorpora la clase `Camioneta` que hereda de `Vehiculo`, con propiedad `NumeroEjes` y métodos sobrescritos. Se actualizan los archivos `.csproj` y `.slnx` para incluir el nuevo archivo y asignar un identificador al proyecto.

---
+++ EXAMEN-POO/Camioneta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXAMEN_POO
+{
+    internal class Camioneta: Vehiculo
+    {
+        public int NumeroEjes;
+
+        public int NumeroEjes
+        {
+            get { return NumeroEjes; }
+            set { NumeroEjes = value; }
+        }
+
+        public Camioneta(int horas, int ejes) : base(horas)
+        {
+            this.NumeroEjes = ejes;
+        }
+
+        public Camioneta(): base()
+        {
+            NumeroEjes = 2;
+        }
+
+
+        public override double CalcularTarifa()
+        {
+            return horasEstancia = 45.0;
+        }
+
+        public override string Describir()
+        {
+            return $"Camioneta con {NumeroEjes} ejes, {horasEstancia} horas de estancia:";
+        }
+    }
+}
